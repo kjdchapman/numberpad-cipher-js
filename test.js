93bb257a54bcd68f1711d765f3bad15b6ed9e826
Sam <samuelbrown201195@hotmail.co.uk>
2020-11-09 12:30:36 +0000

    initial investigation into code starting with 1

--- test.js
+++ test.js
@@ -1,6 +1,35 @@
 var assert = require('assert')
 
 describe('Encryption', () => {
+  describe('2 length code starting with 1', () => {
+    it('returns 55 when given 11', () => {
+      assert.equal(encrypt("11"), "55")
+    })
+    it('returns 56 when given 12', () => {
+      assert.equal(encrypt("12"), "56")
+    })
+    it('returns 54 when given 13', () => {
+      assert.equal(encrypt("13"), "54")
+    })
+    it('returns 58 when given 14', () => {
+      assert.equal(encrypt("14"), "58")
+    })
+    it('returns 59 when given 15', () => {
+      assert.equal(encrypt("15"), "59")
+    })
+    it('returns 57 when given 16', () => {
+      assert.equal(encrypt("16"), "57")
+    })
+    it('returns 52 when given 17', () => {
+      assert.equal(encrypt("17"), "52")
+    })
+    it('returns 53 when given 18', () => {
+      assert.equal(encrypt("18"), "53")
+    })
+    it('returns 51 when given 19', () => {
+      assert.equal(encrypt("19"), "51")
+    })
+  })
   it('returns empty string when given empty string', () => {
     assert.equal(encrypt(""), "")
   })
@@ -13,21 +42,10 @@ describe('Encryption', () => {
     assert.equal(encrypt("2"), "6")
   })
 
-  it('returns 55 when given 11', () => {
-    assert.equal(encrypt("11"), "55")
-  })
-
   it('returns 55 when given 22', () => {
     assert.equal(encrypt("22"), "65")
   })
 
-  it('returns 56 when given 12', () => {
-    assert.equal(encrypt("12"), "56")
-  })
-
-  it('returns 56 when given 13', () => {
-    assert.equal(encrypt("13"), "54")
-  })
 })
 
 describe('Decryption', () => {
@@ -46,6 +64,12 @@ const encrypt = (input) => {
     if (input[1] == "1") output += "5"
     if (input[1] == "2") output += "6"
     if (input[1] == "3") output += "4"
+    if (input[1] == "4") output += "8"
+    if (input[1] == "5") output += "9"
+    if (input[1] == "6") output += "7"
+    if (input[1] == "7") output += "2"
+    if (input[1] == "8") output += "3"
+    if (input[1] == "9") output += "1"
   }
 
   if (input == "22") output = "65"
