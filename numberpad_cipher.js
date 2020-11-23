7c33c3738bdadbb4ab77dfd29e93a3d52c6b1db3
95-Samb <samuelbrown201195@hotmail.co.uk>
2020-11-23 12:55:03 +0000

    moved 123 encoder to seperate file

--- numberpad_cipher.js
+++ numberpad_cipher.js
@@ -43,41 +43,5 @@ exports.encrypt = (input) => {
   return output
 }
 
-exports.smallEncrypt = (input) => {
-  output = ""
-  numberpad = ["1","2","3"]
-  const mod = (x, n) => (x % n + n) % n
-
-  var left = "1"
-  var nomove = "2"
-  var right = "3"
-
-  encoder = (startingPostion, endPosition) => {
-    var number = numberpad.indexOf(startingPostion);
-
-    postionEnconder = new Map([
-      [numberpad[mod((number + 1),3)],right],
-      [numberpad[mod((number - 1),3)],left],
-      [numberpad[number],nomove]
-      ]);
-
-    return postionEnconder.get(endPosition)
-  }
-
-  if (input[0]) {
-    output += encoder("1",input[0])
-  }
-
-  i = 0
-  while (i < input.length - 1) {
-
-    output += encoder(input[i],input[i + 1])
-
-    i++
-  }
-
-  return output
-}
-
 
 exports.decrypt = () => ""
